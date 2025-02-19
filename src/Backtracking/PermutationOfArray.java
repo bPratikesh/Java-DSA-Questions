1509ac6118c15dd0ac000743421f5f0e31a30e50
Pratikesh <pratikeshb9@gmail.com>
2025-02-19 23:04:22 +0530

    Backtracking

--- src/Backtracking/PermutationOfArray.java
+++ src/Backtracking/PermutationOfArray.java
@@ -2,12 +2,27 @@ package Backtracking;
 
 public class PermutationOfArray {
     public static void main(String[] args) {
+        int a[] = {6,5,4};
+        permute(a,0, a.length-1);
+    }
 
+    static void permute(int a[], int left, int right){
+        if(left == right){
+            printArray(a);
+            return;
+        }
+
+        for (int i=left; i<=right; i++){
+            swap(a, i, left);
+            permute(a, left+1, right);
+            swap(a, i, left);
+        }
     }
     static void printArray(int[] a){
         for (int e: a){
             System.out.print(e+" ");
         }
+        System.out.println();
     }
     static void swap(int[] a, int i, int j){
         int temp = a[i];
