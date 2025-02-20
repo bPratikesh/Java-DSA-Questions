0d3af95c12e0301d0d1e8a670ad1bb1cbab02de5
Pratikesh <pratikeshb9@gmail.com>
2025-02-20 17:35:55 +0530

    Backtracking

---
+++ src/Backtracking/GenerateParenthesis.java
@@ -0,0 +1,28 @@
+package Backtracking;
+
+public class GenerateParenthesis {
+    public static void main(String[] args) {
+        int n=3;
+        generateParenthesis("", n, 0, 0, 0);
+    }
+
+     static void generateParenthesis(String cur, int n, int index, int open, int close){
+        if (index == 2*n){
+            System.out.println(cur);
+            return;
+        }
+
+        if(open<n){
+            cur = cur+"(";
+            generateParenthesis(cur, n, index+1, open+1, close);
+            cur=cur.substring(0, cur.length()-1);
+        }
+
+        if(close<open){
+            cur = cur+")";
+            generateParenthesis(cur, n, index+1, open, close+1);
+        }
+
+     }
+
+}
