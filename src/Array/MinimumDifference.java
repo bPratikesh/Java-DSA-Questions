9ba2763f4f4b5bc8ca7de53ce52203e336b68e30
Pratikesh <pratikeshb9@gmail.com>
2025-12-22 22:25:51 +0530

    Minimum difference Array

--- src/Array/MinimumDifference.java
+++ src/Array/MinimumDifference.java
@@ -16,7 +16,6 @@ public class MinimumDifference {
                 minDiff = arr[i]-arr[i-1];
             }
         }
-
         for (int i=1; i<arr.length; i++){
             if(arr[i]-arr[i-1] == minDiff) {
                 List<Integer> temp = new ArrayList<>();
