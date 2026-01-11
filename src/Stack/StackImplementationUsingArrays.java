fd24221d06664c4ea9af4875e18070fc95b6ba89
Pratikesh <pratikeshb9@gmail.com>
2026-01-11 22:34:55 +0530

    Stack implementation using an array

---
+++ src/Stack/StackImplementationUsingArrays.java
@@ -0,0 +1,83 @@
+package Stack;
+
+public class StackImplementationUsingArrays {
+    public static void main(String[] args) {
+        StackImplementationUsingArrays stack = new StackImplementationUsingArrays(4);
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+//        stack.printStack();
+//        stack.push(1);
+//        stack.printStack();
+//        stack.pop();
+//        stack.printStack();
+//        System.out.println(stack.peek());
+
+
+        //Iterating stack
+        while (!stack.isStackEmpty()){
+            System.out.print(stack.pop()+" ");
+        }
+    }
+
+    int size;
+    int a[];
+    int top;
+
+    public StackImplementationUsingArrays(int size){
+        this.size = size;
+        a = new int[size];
+        top = -1;
+    }
+
+    boolean push(int data){
+        if(isStackFull()){
+            System.out.println("Stack Overflow");
+            return false;
+        }else{
+            top++;
+            a[top] = data;
+            return true;
+        }
+    }
+
+    int pop(){
+        if(isStackEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }else{
+            int temp = a[top];
+            top--;
+            return temp;
+        }
+    }
+
+    int peek(){
+        if (isStackEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        else{
+            return a[top];
+        }
+    }
+
+    boolean isStackEmpty(){
+        return top == -1;
+    }
+
+    boolean isStackFull(){
+        return top == size-1;
+    }
+
+    void printStack(){
+        for (int i=0; i<=top; i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+
+}
